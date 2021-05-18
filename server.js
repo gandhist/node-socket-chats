221312fde9de22a134ee20ddf6e52bfd2d726364
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2021-05-18 15:52:44 +0700

    Add CORS to socket initialization

--- server.js
+++ server.js
@@ -34,7 +34,12 @@ var allowCrossDomain = (req, res, next) => {
 }   
 app.use(allowCrossDomain);
 const server = http.createServer(app)
-const io = socketio(server)
+const io = socketio(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"]
+    }
+})
 const chatBot = "🤖Raven"
 
 // set static folder for development purpose only
