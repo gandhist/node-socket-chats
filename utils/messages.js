61c4632da16765eb09fb6424a5a342927cd6616b
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2021-05-19 19:38:11 +0700

    format message sender by jadi int

--- utils/messages.js
+++ utils/messages.js
@@ -3,7 +3,7 @@ const moment = require('moment')
 
 function formatMessage(send_by = '', username, text, type_message = '', group_id = '', room_id, time = moment().format('h:mm a')) {
     return {
-        send_by: send_by,
+        send_by: parseInt(send_by),
         sender_name: username,
         message: text,
         type_message: type_message,
