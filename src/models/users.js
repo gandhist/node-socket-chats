91680544bc8fc380a22983b0bf0ae5f59d079c6e
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2021-05-29 11:01:00 +0700

    Fix operation model yang kehapus

--- src/models/users.js
+++ src/models/users.js
@@ -25,6 +25,17 @@ User.register = function (newUser, result) {
     })
 }
 
+User.getByEmail = function (email, result) {
+    dbConn.query(`select id, name, email, no_hp, tipe_user, token, password, picture from users_chats where email = ? limit 1`, email, function (err, res) {
+        if (err) {
+            result(err, null)
+        }
+        else {
+            result(null, res)
+        }
+    })
+}
+
 User.updateJwt = function (body, result) {
     dbConn.query(`update users_chats set token = ? where id = ?`, [body.token, body.id], function (err, res) {
         if (err) {
