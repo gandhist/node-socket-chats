e60fe5ded7f7a02b12dc26bc9c800caa0c8299b8
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2021-06-16 15:10:58 +0700

    route upload document (all extension)

--- src/controllers/uploads.js
+++ src/controllers/uploads.js
@@ -81,6 +81,70 @@ const image = async (req, res) => {
 
 }
 
+const document = async (req, res) => {
+    const user = req.user;
+    try {
+        if(!req.files) {
+            return res.status(400).send({
+                status: false,
+                errors: 'No file uploaded'
+            });
+        } else {
+            let lampiran = req.files.lampiran;
+
+            if (lampiran === null || typeof lampiran === 'undefined') {
+                return res.status(401).send({
+                    status: false,
+                    errors: 'lampiran field not found'
+                });
+            } 
+        
+            let extension = lampiran.name.split('.')
+            extension = extension[extension.length - 1]
+
+            let filename = "USER_UPLOAD_" + user.id + "_" + new Date().valueOf() + "." + extension
+            let desti = __dirname + "/../../tmp/" + filename
+            lampiran.mv(desti)
+            let uploading = await storage.bucket('csi-absensi').upload(desti, {
+                destination: "chat/documents/" + filename,
+                resumable:false 
+            });
+            fs.unlinkSync(desti)
+
+            if (uploading) {
+                let uri = "https://storage.googleapis.com/" + uploading[0].metadata.bucket + "/" + uploading[0].metadata.name
+                query = `insert into media (file_name, extension, uri, download_uri, size, created_at, created_by) values ('${filename}', '${extension}', '${uri}', '${uploading[0].metadata.mediaLink}', '${uploading[0].metadata.size}', "${moment.utc().format('YYYY-MM-DD HH:mm:ss')}", ${user.id})`;
+    
+                db.query(query);
+                
+                return res.send({
+                    status: true,
+                    message: {
+                        uri :uri, 
+                        donwload_uri :uploading[0].metadata.mediaLink,
+                        type: uploading[0].metadata.contentType
+                    }
+                });
+            }
+
+            return res.status(500).send({
+                status: false,
+                errors: 'Fail to upload document'
+            });
+
+        }
+    } catch (err) {
+        console.log(err)
+        return res.status(500).send({
+            status: false,
+            errors: 'Fail to upload document'
+        });
+    }
+
+}
+
+
 module.exports = {
-    image
+    image,
+    document
 }
